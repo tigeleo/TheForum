1f21d6ce00061dd0bbfd7c2472e73be217320b11
tigeleo <35178296+tigeleo@users.noreply.github.com>
2018-04-11 02:29:40 +0300

    xxx

--- TheForum/src/app/usermessages/usermessages.component.ts
+++ TheForum/src/app/usermessages/usermessages.component.ts
@@ -37,7 +37,8 @@ export class UsermessagesComponent implements OnInit {
         this.loadAllMessages();
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
         this.curUser = this.authenticationService.currentUser;
-        UserMsg userMsg = UserMsg {id: -1, fromUserName: from_un, toUserName: "", title: "", text: "" };
+        let userMsg:UserMsg =  {id: -1, fromUserName: this.curUser.username, toUserName: "", title: "", text: ""  };
+        
         this.model = userMsg;
     }
 
