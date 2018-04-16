f7fb88ed790107496a9d9b01f96b363c3f798959
davidbd <davidbd@IL-DAVBD-X230.Corp.Exlibrisgroup.com>
2018-04-16 08:55:03 +0300

    Commit service

---
+++ TheForum/src/app/userform/userform.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import { User } from '../_models/index';
+import * as myGlobals from '../_helpers/globals';
+
+@Injectable()
+export class UserformService {
+
+    
+  constructor(private http: HttpClient) { }
+
+    updateUser(user: User){
+        return this.http.post<any>(myGlobals.backendApiLinks.userupdate, user)
+            .map(user => {
+    
+                return user;
+            });    
+    }
+    
+
+}
