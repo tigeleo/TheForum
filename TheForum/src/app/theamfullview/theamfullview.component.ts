6efdab84e2853a6e47734c52a0a6d67c431e2d34
Davidbd <Davidbd@IL-DAVBD-X230.Corp.Exlibrisgroup.com>
2018-04-19 23:36:05 +0300

    Last commit

--- TheForum/src/app/theamfullview/theamfullview.component.ts
+++ TheForum/src/app/theamfullview/theamfullview.component.ts
@@ -43,7 +43,7 @@ export class TheamfullviewComponent implements OnInit {
         */
         this.theamfullviewService.getById(id).subscribe(
                 dicussion => { 
-                    debugger;
+                    //debugger;
                     this.theam=dicussion;
                     console.log(this.theam);
                     
